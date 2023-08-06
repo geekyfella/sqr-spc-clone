b5f36cfd4fe5f9c6e0624f9760d35b825920f2da
Ojasvi <ojasvikumar10@gmail.com>
2023-08-06 14:07:15 +0530

    first push

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-gsap.from("#SS-Logo",{
-    y:-100,
-    duration:2,
-    delay:1,
-    opacity:1,
-})
+// gsap.from("#SS-Logo",{
+//     x:10,
+//     duration:2,
+//     delay:1,
+//     opacity:1,
+// })
 
-gsap.from("#nav-bar",{
-    y:100,
-    duration:1,
-    delay:1,
-})
+// gsap.from("#nav-bar",{
+//     y:100,
+//     duration:1,
+//     delay:1,
+// })
