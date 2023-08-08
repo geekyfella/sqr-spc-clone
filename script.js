360257b107da96626a6d670851c2a428ec54327d
Ojasvi <ojasvikumar10@gmail.com>
2023-08-08 09:45:23 +0530

    login page built

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-// gsap.from("#SS-Logo",{
-//     x:10,
-//     duration:2,
-//     delay:1,
-//     opacity:1,
-// })
+gsap.from("#SS-Logo",{
+    y:-10,
+    duration:0.5,
+    delay:0.5,
+    opacity:0,
+})
 
-// gsap.from("#nav-bar",{
-//     y:100,
-//     duration:1,
-//     delay:1,
-// })
+gsap.from("#child-nav01",{
+    y:-10,
+    duration:0.5,
+    delay:0.5,
+    opacity:0,
+})
+
+gsap.from("#child-nav02",{
+    y:-10,
+    duration:0.5,
+    delay:0.5,
+    opacity:0,
+})
+
+gsap.from("#center-text",{
+    y:-10,
+    duration:0.5,
+    delay:1,
+    opacity:0,
+})
+
+gsap.from("#kathak-img",{
+    x:-10,
+    duration:0.5,
+    delay:1.3,
+    opacity:0,
+})
+
+gsap.from("#small-business-img",{
+    y:10,
+    duration:0.5,
+    delay:1.3,
+    opacity:0,
+})
