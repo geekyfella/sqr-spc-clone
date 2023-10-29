b95169e1378c9f2ba94941ca229af4d3b5820a53
Ojasvi <ojasvikumar10@gmail.com>
2023-10-29 20:51:53 +0530

    update in comments

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
-gsap.from("#SS-Logo",{
+var tl = gsap.timeline()
+
+
+tl.from("#SS-Logo",{
     y:-10,
     duration:0.5,
-    delay:0.5,
+    delay:0,
     opacity:0,
+    stagger:0.3,
 })
 
-gsap.from("#child-nav01",{
+tl.from("#child-nav01",{
     y:-10,
     duration:0.5,
-    delay:0.5,
+    delay:0,
     opacity:0,
+    stagger:0.3,
 })
 
-gsap.from("#child-nav02",{
+tl.from("#child-nav02",{
     y:-10,
     duration:0.5,
-    delay:0.5,
+    delay:0,
     opacity:0,
+    stagger:0.3,
 })
 
-gsap.from("#center-text",{
-    y:-10,
+tl.from("#everything, #tobooknowsell, #anything",{
+    y:100,
     duration:0.5,
-    delay:1,
+    delay:0,
     opacity:0,
+    stagger:0.3,
 })
 
-gsap.from("#kathak-img",{
+tl.from("#kathak-img",{
     x:-10,
     duration:0.5,
-    delay:1.3,
+    delay:0,
     opacity:0,
 })
 
-gsap.from("#small-business-img",{
-    y:10,
+tl.from("#small-business-img",{
+    x:10,
     duration:0.5,
-    delay:1.3,
+    delay:0,
     opacity:0,
+    // scrollTrigger:{
+    //     trigger:"#small-business-img",
+    //     scroller:"body",
+    //     scrub:true,
+    //     markers:false,
+    // }
 })
